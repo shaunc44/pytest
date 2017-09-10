31a6dcf7d3900142cdabc0f78dd031ea82bd0c4a
Shaun <seyyes2000@hotmail.com>
2017-09-10 15:49:46 -0400

    set up pytests

---
+++ math.py
@@ -0,0 +1,20 @@
+def add(a, b):
+    '''
+    >>> add(5, 3)
+    8
+    >>> add('he', 'llo')
+    'hello'
+    '''
+    return a + b
+
+def subtract(a, b):
+    '''
+    >>> subtract(20, 16)
+    4
+    >>> subtract(6.5, 4.3)
+    2.2
+    '''
+    return a - b
+
+def multiply(a, b):
+
